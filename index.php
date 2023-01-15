225ffff3a4e61ff8d037cc37005f08472b63a0b5
Youssef tdl <usftadlaoui@gmail.com>
2023-01-15 22:23:56 +0100

    class index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once './controllers/HomeController.php';
+$home= new HomeController();
+$pages= ['add','update','home','delete'];
+if(isset($_GET['page'])){
+    if(in_array($_GET['page'],$pages)){
+        $page=$_GET['page'];
+        $home->index($page);
+    }else{
+     include('views/includes/404.php');
+    }
+}else{
+    $home->index('home');
+}
+
+?>
